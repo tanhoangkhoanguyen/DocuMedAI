34f547f14abef06749218a5a02cbad3a4ac6f3f9
tanhoangkhoanguyen <hoangkhoa.nguyentan@gmail.com>
2025-08-01 10:30:47 +0700

    Done chatbot_w_qdrant. No re-ranking

--- backend/playground/khoanth/RAG/law_retriever.py
+++ backend/playground/khoanth/RAG/law_retriever.py
@@ -1,17 +1,5 @@
-# from dotenv import load_dotenv
-# from langchain_core.tools import tool
-
-# load_dotenv()
-
-# @tool
-# def meh(query):
-#     """
-#     Test `@tool`
-#     """
-#     print ("urgh")
-
-# meh("Nhi")
 from playground.khoanth.RAG.tools import law_classifier, multi_query, step_back, tavily_search, retrieve_doc
+from playground.khoanth.prompts import CONTEXT_QUESTION_PROMPT
 
 import asyncio, warnings, time
 
@@ -24,16 +12,15 @@ async def invoke_law_advisor(llm, structured_llm, message):
     2. generate response
     """
     start_time = time.time()
-    law_type = "criminal_law"#law_classifier(structured_llm, message)
-    # multi_query_resp, step_back_resp, tavily_resp = await asyncio.gather(
-    #     multi_query(llm, message),
-    #     step_back(llm, message),
-    #     tavily_search(message)
-    # )
+    law_type = law_classifier(structured_llm, message)
+    multi_query_resp, step_back_resp, tavily_resp = await asyncio.gather(
+        multi_query(llm, message),
+        step_back(llm, message),
+        tavily_search(message)
+    )
 
     
-    queries = ['What is the potential prison sentence for stealing $500?', 'How long could I expect to be incarcerated for a theft of $500?', 'What are the typical legal consequences for stealing an amount of $500?', 'what are the potential legal consequences for theft?']#multi_query_resp + [step_back_resp]
-    # docs = [retrieve_doc(query, law_type) for query in queries]
+    queries = multi_query_resp + [step_back_resp]
     docs = await asyncio.gather(*(retrieve_doc(q, law_type) for q in queries))
     reliable_docs = []
     unreliable_docs = []
@@ -43,15 +30,11 @@ async def invoke_law_advisor(llm, structured_llm, message):
                 reliable_docs.append(doc.payload['text'])
             else:
                 unreliable_docs.append(doc.payload['text'])
-    print (reliable_docs)
-    print ()
-    print (unreliable_docs)
-    print (time.time() - start_time)
-
-    return "yNhi dth"
-
-# filtered = [
-#                 res.payload['text']
-#                 for res in results
-#                 if res.score >= threshold
-#             ]
+    
+    prompt = CONTEXT_QUESTION_PROMPT.format(
+        reliable_context = reliable_docs,
+        unreliable_context = unreliable_docs,
+        website_information = tavily_resp,
+        question = message
+    )
+    return llm.invoke(prompt).content
