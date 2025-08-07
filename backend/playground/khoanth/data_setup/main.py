87273d9d170a6720913156d1e5060222ccb0ded9
tanhoangkhoanguyen <hoangkhoa.nguyentan@gmail.com>
2025-08-07 18:10:26 +0700

    Fix elastic data upload

--- backend/playground/khoanth/data_setup/main.py
+++ backend/playground/khoanth/data_setup/main.py
@@ -2,5 +2,5 @@ from playground.khoanth.data_setup.qdrant import qdrant_setup
 from playground.khoanth.data_setup.elastic import elastic_setup
 
 if __name__ == "__main__":
-    # qdrant_setup()
+    qdrant_setup()
     elastic_setup()
